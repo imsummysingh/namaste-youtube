b60ad6b4ce91baf164e7ccdc9be9fbd1ac6f7dba
Summy Singh <summysingh86@gmail.com>
2024-03-28 01:06:56 +0530

    search & Comment feature added

--- namaste-youtube/src/components/Head.js
+++ namaste-youtube/src/components/Head.js
@@ -1,9 +1,65 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cachedResults } from "../utils/searchSlice";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSugestions, setShowSuggestions] = useState(false);
+  const [hide, setHide] = useState(true);
+
+  const searchCache = useSelector((store) => store.search);
   const dispatch = useDispatch();
+
+  //Logic for hiding suggestion on scrolling
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.onscroll = () => {
+        let currentScrollPos = window.scrollY;
+        let maxScroll = document.body.scrollHeight - window.innerHeight;
+        // console.log(maxScroll)
+        if (currentScrollPos > 0 && currentScrollPos < maxScroll) {
+          setHide(false);
+          // console.log(currentScrollPos)
+        } else {
+          setHide(true);
+        }
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    //new timer every 200ms
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    //console.log("API CALL - " + searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    //console.log(json[1]);
+    setSuggestions(json[1]);
+
+    //update cache
+    dispatch(
+      cachedResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
+
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
@@ -24,14 +80,36 @@ const Head = () => {
           />
         </a>
       </div>
-      <div className="col-span-10 px-30">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-        />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-200">
-          🔍
-        </button>
+      <div className="col-span-10 px-10">
+        <div>
+          <input
+            className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => {
+              setShowSuggestions(false);
+            }}
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-200">
+            🔍
+          </button>
+        </div>
+        {showSugestions && hide && (
+          <div
+            id="suggestionBox"
+            className="fixed bg-white py-2 px-2 w-[31.5rem] shadow-lg rounded-lg border border-gray-100"
+          >
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img
--- namaste-youtube/src/components/VideoCard.js
+++ namaste-youtube/src/components/VideoCard.js
@@ -15,4 +15,13 @@ const VideoCard = ({ info }) => {
   );
 };
 
+//Higher order component -  Takes in a component and modify a little and return the component
+export const AdVideoCard = ({ info }) => {
+  return (
+    <div className="p-1 m-1 border border-red-900">
+      <VideoCard info={info} />
+    </div>
+  );
+};
+
 export default VideoCard;
